b1c53daee08ed411729cfeccf0518a70d1b806a5
Emmanuil Vardanian <em.vardany@gmail.com>
2022-09-07 14:54:19 +0300

    change file

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.get('/', (req, res, next) => {
 console.log(123456)
 router(app);
 
-app.listen(port, async () =>{
-    console.log(`Server started on port ${port}`);
-    await db.connect();
-})
+// app.listen(port, async () =>{
+//     console.log(`Server started on port ${port}`);
+//     await db.connect();
+// })
 
 
